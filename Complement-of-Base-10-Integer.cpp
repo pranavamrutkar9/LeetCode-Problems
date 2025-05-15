f9c1728119f943154dc469fbf24ab88a5b1a8f73
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-05-15 14:37:56 +0530

    [1009] [Time Beats: 100.00%] [Memory Beats: 90.39%] - LeetPush

---
+++ Complement-of-Base-10-Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int temp = n;
+        int count = 0;
+        if(n==0){
+            return 1;
+        }
+        while (temp != 0) {
+            count++;
+            temp = temp >> 1;
+        }
+        int mask = (1 << count) - 1;
+        return (~(n)&mask);
+    }
+};
